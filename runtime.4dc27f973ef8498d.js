faec5df91b0aa5a56194d88323eff909055cb402
Cacildovaldo <123966041+Cacildovaldo@users.noreply.github.com>
2024-07-27 04:13:34 -0300

    runtime.4dc27f973ef8498d.js

--- runtime.4dc27f973ef8498d.js
+++ runtime.4dc27f973ef8498d.js
@@ -1,172 +1,168 @@
 (() => {
   "use strict";
+  var __webpack_modules__ = ({
+    "./src/index.js": (module, __webpack_exports__, __webpack_require__) => {
+      // Define the module
+      __webpack_require__.r(__webpack_exports__);
+      var _dependency_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./src/dependency.js");
+      _dependency_js__WEBPACK_IMPORTED_MODULE_0__.default();
+    },
+    "./src/dependency.js": (module, __webpack_exports__, __webpack_require__) => {
+      // Define the module
+      __webpack_require__.r(__webpack_exports__);
+      __webpack_exports__.default = () => {
+        console.log("Dependency loaded");
+      };
+    },
+    "./src/index.css": (module, __webpack_exports__, __webpack_require__) => {
+      // Define the module
+      __webpack_require__.r(__webpack_exports__);
+    }
+  });
 
-  var modules = {};
-  var moduleCache = {};
+  var __webpack_module_cache__ = {};
 
-  function require(moduleId) {
-    var cachedModule = moduleCache[moduleId];
+  function __webpack_require__(moduleId) {
+    var cachedModule = __webpack_module_cache__[moduleId];
     if (cachedModule !== undefined) {
       return cachedModule.exports;
     }
-    var module = moduleCache[moduleId] = {
-      id: moduleId,
-      loaded: false,
+
+    var module = __webpack_module_cache__[moduleId] = {
       exports: {}
     };
 
-    modules[moduleId].call(module.exports, module, module.exports, require);
+    __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 
-    module.loaded = true;
     return module.exports;
   }
 
-  require.m = modules;
-  require.amdO = {};
-
-  var deferred = [];
-  require.O = (result, chunkIds, fn, priority) => {
-    if (!chunkIds) {
-      var highestPriority = 1 / 0;
-      for (var i = 0; i < deferred.length; i++) {
-        var [chunkIds, fn, priority] = deferred[i];
-        var allLoaded = true;
-        for (var j = 0; j < chunkIds.length; j++) {
-          if ((priority & 1) === 0 || highestPriority >= priority) {
-            if (Object.keys(require.O).every((key) => require.O[key](chunkIds[j]))) {
-              chunkIds.splice(j--, 1);
-            } else {
-              allLoaded = false;
-              if (priority < highestPriority) {
-                highestPriority = priority;
-              }
-            }
-          }
-        }
-        if (allLoaded) {
-          deferred.splice(i--, 1);
-          var result = fn();
-          if (result !== undefined) result = result;
-        }
+  (() => {
+    __webpack_require__.r = (exports) => {
+      if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+        Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
       }
-      return result;
-    }
-    priority = priority || 0;
-    for (var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) {
-      deferred[i] = deferred[i - 1];
-    }
-    deferred[i] = [chunkIds, fn, priority];
-  };
-
-  require.n = (module) => {
-    var getter = module && module.__esModule ? () => module.default : () => module;
-    require.d(getter, { a: getter });
-    return getter;
-  };
-
-  require.t = (value, mode) => {
-    if (mode & 1) value = this(value);
-    if (mode & 8) return value;
-    if (mode & 4 && typeof value === "object" && value && value.__esModule) return value;
-        var ns = Object.create(null);
-    require.r(ns);
-    Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-    if (mode & 2 && typeof value != 'string')
-      for (var key in value)
-        require.d(ns, key, function (key) { return value[key]; }.bind(null, key));
-    return ns;
-  };
-
-  require.d = (exports, definition) => {
-    for (var key in definition) {
-      if (require.o(definition, key) && !require.o(exports, key)) {
-        Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-      }
-    }
-  };
-
-  require.f = {};
-  require.e = (chunkId) => {
-    return Promise.all(Object.keys(require.f).reduce((promises, key) => {
-      require.f[key](chunkId, promises);
-      return promises;
-    }, []));
-  };
-
-  require.u = (chunkId) => {
-    return "" + chunkId + ".js";
-  };
-
-  require.miniCssF = (chunkId) => {
-    return "" + chunkId + ".css";
-  };
-
-  require.g = (function () {
-    if (typeof globalThis === 'object') return globalThis;
-    try {
-      return this || new Function('return this')();
-    } catch (e) {
-      if (typeof window === 'object') return window;
-    }
+      Object.defineProperty(exports, '__esModule', { value: true });
+    };
   })();
 
-  require.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
+  (() => {
+    __webpack_require__.d = (exports, definition) => {
+      for (var key in definition) {
+        if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+          Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+        }
+      }
+    };
+  })();
 
-  require.r = (exports) => {
-    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-      Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+  (() => {
+    __webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
+  })();
+
+  (() => {
+    __webpack_require__.n = (module) => {
+      var getter = module && module.__esModule ?
+        () => module['default'] :
+        () => module;
+      __webpack_require__.d(getter, { a: getter });
+      return getter;
+    };
+  })();
+
+  (() => {
+    __webpack_require__.e = (chunkId) => {
+      return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
+        __webpack_require__.f[key](chunkId, promises);
+        return promises;
+      }, []));
+    };
+  })();
+
+  (() => {
+    __webpack_require__.u = (chunkId) => {
+      return "" + chunkId + ".js";
+    };
+  })();
+
+  (() => {
+    __webpack_require__.p = "";
+  })();
+
+  var installedChunks = {
+    "runtime": 0
+  };
+
+  var deferred = [];
+
+  function webpackJsonpCallback(parentChunkLoadingFunction, data) {
+    var [chunkIds, moreModules, runtime] = data;
+    var moduleId, chunkId, i = 0;
+
+    if (chunkIds.some((id) => (installedChunks[id] !== 0))) {
+      for (moduleId in moreModules) {
+        if (__webpack_require__.o(moreModules, moduleId)) {
+          __webpack_modules__[moduleId] = moreModules[moduleId];
+        }
+      }
+      if (runtime) var result = runtime(__webpack_require__);
     }
-    Object.defineProperty(exports, '__esModule', { value: true });
-  };
 
-  require.nmd = (module) => {
-    module.paths = [];
-    if (!module.children) module.children = [];
-    return module;
-  };
+    if (parentChunkLoadingFunction) parentChunkLoadingFunction(data);
 
-  require.p = "";
-
-  var jsonpArray = (typeof self !== 'undefined' ? self : this)["webpackChunk"] = (typeof self !== 'undefined' ? self : this)["webpackChunk"] || [];
-  var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
-  jsonpArray.push = webpackJsonpCallback;
-  jsonpArray = jsonpArray.slice();
-  for (var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
-  var parentJsonpFunction = oldJsonpFunction;
-
-  function webpackJsonpCallback(data) {
-    var [chunkIds, moreModules, runtime, executeModules] = data;
-    var moduleId, chunkId, i = 0, resolves = [];
     for (; i < chunkIds.length; i++) {
       chunkId = chunkIds[i];
-      if (require.o(installedChunks, chunkId) && installedChunks[chunkId]) {
-        resolves.push(installedChunks[chunkId][0]);
+      if (__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+        installedChunks[chunkId][0]();
       }
       installedChunks[chunkId] = 0;
     }
-    for (moduleId in moreModules) {
-      if (require.o(moreModules, moduleId)) {
-        require.m[moduleId] = moreModules[moduleId];
-      }
-    }
-    if (runtime) var result = runtime(require);
-    if (parentJsonpFunction) parentJsonpFunction(data);
 
-    while (resolves.length) {
-      resolves.shift()();
+    while (deferred.length) {
+      deferred.shift()();
     }
-
-    if (executeModules) deferred.push.apply(deferred, executeModules);
 
     return result;
   }
 
-  var installedChunks = {
-    "main": 0
+  var chunkLoadingGlobal = self["webpackChunk"] = self["webpackChunk"] || [];
+  chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+  chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+
+  __webpack_require__.O = (result, chunkIds, fn, priority) => {
+    if (chunkIds) {
+      priority = priority || 0;
+      for (var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+      deferred[i] = [chunkIds, fn, priority];
+      return;
+    }
+
+    var notFulfilled = Infinity;
+    for (var i = 0; i < deferred.length; i++) {
+      var [chunkIds, fn, priority] = deferred[i];
+      var fulfilled = true;
+      for (var j = 0; j < chunkIds.length; j++) {
+        if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every((key) => (__webpack_require__.O[key](chunkIds[j])))) {
+          chunkIds.splice(j--, 1);
+        } else {
+          fulfilled = false;
+          if (priority < notFulfilled) notFulfilled = priority;
+        }
+      }
+      if (fulfilled) {
+        deferred.splice(i--, 1)
+        var r = fn();
+        if (r !== undefined) result = r;
+      }
+    }
+
+    return result;
   };
 
-  require.O(undefined, ["styles"], () => (require("./src/index.js")));
-  var __webpack_exports__ = require.O(undefined, ["styles"], () => (require("./src/styles.css")));
-  __webpack_exports__ = require.O(__webpack_exports__);
-})();
+  __webpack_require__.O.j = (chunkId) => (installedChunks[chunkId] === 0);
 
+  __webpack_require__.O(undefined, ["main"], () => (__webpack_require__("./src/index.js")));
+  var __webpack_exports__ = __webpack_require__.O(undefined, ["main"], () => (__webpack_require__("./src/index.css")));
+  __webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+
+})();
